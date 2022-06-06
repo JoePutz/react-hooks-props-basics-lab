d29bf279b8b952d3245a48d5cbdc60304043d1b1
Joseph <josephputz89@gmail.com>
2022-06-06 18:32:30 -0500

    commit tree e43384804c6dc5d9136843fe689bc5e97aa6bb5b

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Home(props) {
+  // console.log(props);
   return (
     <div id="home">
       <h1 style={{ color: props.color }}>
@@ -9,5 +10,17 @@ function Home(props) {
     </div>
   );
 }
+
+
+
+// function Home(props) {
+//   return (
+//     <div id="home">
+//       <h1 style={{ color: props.color }}>
+//         {props.name} is a Web Developer from {props.city}
+//       </h1>
+//     </div>
+//   );
+// }
 
 export default Home;
